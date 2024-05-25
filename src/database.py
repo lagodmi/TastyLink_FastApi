1bf363521a35bfb5ea2f7d0dc1bc69f7af8385a0
Dmitrii Lagodnov <lagodnov@gmail.com>
2024-05-25 23:28:06 +0300

    fix

--- src/database.py
+++ src/database.py
@@ -1,4 +1,9 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    declared_attr
+)
 from sqlalchemy.ext.asyncio import (
     create_async_engine,
     async_sessionmaker,
